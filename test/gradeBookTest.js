69dea204d4eb618b3422993c378a6bd589d0ecb6
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-28 22:38:12 +0200

    created test "Should add new grade and return count of 1"

--- test/gradeBookTest.js
+++ test/gradeBookTest.js
@@ -1,1 +1,7 @@
 var gradeBook = require('../lib/gradeBook').gradeBook;
+
+exports["Should add a new grade and return count of 1"] = function(test) {
+    gradeBook.addGrade(90);
+    test.equal(gradeBook.getCountOfGrades(), 1);
+    test.done();
+}

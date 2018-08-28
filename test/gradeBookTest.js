0baef9d86b6cc37085bdcbd57449045ad41b72d7
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-28 22:42:32 +0200

    Create test "Should calculate and return the average of grades"

--- test/gradeBookTest.js
+++ test/gradeBookTest.js
@@ -4,4 +4,11 @@ exports["Should add a new grade and return count of 1"] = function(test) {
     gradeBook.addGrade(90);
     test.equal(gradeBook.getCountOfGrades(), 1);
     test.done();
-}
+};
+
+exports["Should calculate and return the average of grades"] = function(test) {
+    gradeBook.addGrade(100);
+    gradeBook.addGrade(50);
+    test.equal(gradeBook.getAverage(), 75);
+    test.done();
+};

ed61b227b1a00c6cb96604185a93a3bf717295fd
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-28 22:51:45 +0200

    added setUp to setup variables for tests

--- test/gradeBookTest.js
+++ test/gradeBookTest.js
@@ -1,5 +1,10 @@
 var gradeBook = require('../lib/gradeBook').gradeBook;
 
+exports["setUp"] = function(callback) {
+    gradeBook.reset();
+    callback();
+};
+
 exports["Should add a new grade and return count of 1"] = function(test) {
     gradeBook.addGrade(90);
     test.equal(gradeBook.getCountOfGrades(), 1);

f848215295f04713f178da0472865965b2aa79b7
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-28 23:43:38 +0200

    changed index.js to responde to /grade

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ app.get("/", function(req, res) {
     res.send("Hello world!");
 });
 
+app.get("/grade", function(req, res) {
+    var grades = req.query.grades.split(",");
+    
+    for(var i = 0; i < grades.length; i++) {
+        gradeBook.addGrade(parseInt(grades[i]));
+    }
+
+    res.send("Your average grade is " + gradeBook.getAverage() + " (" + gradeBook.getLetterGrade() + ")");
+
+});
+
 app.listen(3000);
 console.log("Server is running and listening to port 3000...");

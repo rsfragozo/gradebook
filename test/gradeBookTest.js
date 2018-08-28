c654cf869b905b5df566352c300647d13829e27b
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-28 23:10:36 +0200

    Create test to return letter grade and updated tests descriptions

--- test/gradeBookTest.js
+++ test/gradeBookTest.js
@@ -11,9 +11,16 @@ exports["Should add a new grade and return count of 1"] = function(test) {
     test.done();
 };
 
-exports["Should calculate and return the average of grades"] = function(test) {
+exports["Should return the average of 75"] = function(test) {
     gradeBook.addGrade(100);
     gradeBook.addGrade(50);
     test.equal(gradeBook.getAverage(), 75);
+    test.done();
+};
+
+exports["Should return letter grade A"] = function(test) {
+    gradeBook.addGrade(100);
+    gradeBook.addGrade(90);
+    test.equal(gradeBook.getLetterGrade(), "A");
     test.done();
 };
